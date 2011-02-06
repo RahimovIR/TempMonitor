0d7a8464d102de01ebf38899e08a7714319e989e
Rakhimov Ilyas <rahimovir@gmail.com>
2011-02-06 16:43:17 +0500

    fix bug

--- src/bot/plugins/temp.py
+++ src/bot/plugins/temp.py
@@ -5,6 +5,6 @@ def init():
     return 0
 
 def run(bot, mess):
-    alert_env = imp.load_source('alert_env', '../../alert_env.py')
+    alert_env = imp.load_source('alert_env', '../alert_env.py')
     text = alert_env.GetTemp()
     bot.send(xmpp.Message(mess.getFrom(), text))
