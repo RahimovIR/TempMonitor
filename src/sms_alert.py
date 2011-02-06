08041d968b6bd45ed9a7116d9fde867d5b50eb35
Rakhimov Ilyas <rahimovir@gmail.com>
2011-02-06 15:26:35 +0500

    Исправил текст смса

--- src/sms_alert.py
+++ src/sms_alert.py
@@ -7,8 +7,8 @@ def SendTempTo(num = '+79199230235', force = False):
     curTemp = alert_env.GetTemp(0)
     if (alert_env.HighTempLevel < curTemp[1]) or (curTemp[1] < alert_env.LowTempLevel) or force:
         print "send sms to " + str(num)
-        time = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
-        alert_env.SendSMS(num, time + ' Temperatura: ' + str(curTemp))
+        time = curTemp[0].strftime("%d/%m/%y %H:%M:%S")
+        alert_env.SendSMS(num, time + ' Temperatura: ' + str(curTemp[1]))
 
 if __name__ == "__main__":
     SendTempTo(num = "+79199230235")
